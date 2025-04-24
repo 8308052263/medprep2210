f2262e955a454edcf328f86384d14edbda79ba65
Geeta Virwani <drgeet@Geetas-MacBook-Air.local>
2025-04-24 07:36:44 +0530

    Fixed router setup with BrowserRouter and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import Dashboard from './components/Dashboard'
-import Flashcards from './components/Flashcards'
-import MCQs from './components/MCQs'
-import Login from './components/Login'
-import Signup from './components/Signup'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Dashboard from './components/Dashboard';
+import Flashcards from './components/Flashcards';
+import MCQs from './components/MCQs';
 
 function App() {
   return (
-    <div className="flex h-screen">
+    <div className="flex">
       <Sidebar />
-      <div className="flex flex-col flex-1">
+      <div className="flex-1">
         <Navbar />
-        <div className="p-4">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/flashcards" element={<Flashcards />} />
-            <Route path="/mcqs" element={<MCQs />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/flashcards" element={<Flashcards />} />
+          <Route path="/mcqs" element={<MCQs />} />
+        </Routes>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
